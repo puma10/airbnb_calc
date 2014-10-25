2c4e6ae584021334dd6aab278602d7b7f74899b5
Josh Wardini <joshwardini@gmail.com>
2014-10-25 15:31:38 +0800

    All fields are connected.  Output is incorrect

--- my_app/static/js/main.js
+++ my_app/static/js/main.js
@@ -7,21 +7,54 @@ $(document).ready(function() {
 
     console.log("the form has been submitted");
 
-    // grab values
+    // grab values from the form
     title_form = $('input[name="title"]').val();
     rent_form = $('input[name="rent"]').val();
-    console.log(title_form, rent_form)
+    water_form = $('input[name="water"]').val();
+    sewer_form = $('input[name="sewer"]').val();
+    garbage_form = $('input[name="garbage"]').val();
+    electric_form = $('input[name="electric"]').val();
+    cable_form = $('input[name="cable"]').val();
+    maid_form = $('input[name="maid_service"]').val();
+    hotel_tax_form = $('input[name="hotel_tax"]').val();
 
-    //sends the POST data back to the server from the client using AJAX
+    occupancy_form = $('input[name="occupancy"]').val();
+    daily_price_form = $('input[name="daily_price"]').val();
+
+    submit_time = new Date($.now());
+
+    console.log(title_form,
+                rent_form,
+                water_form,
+                sewer_form,
+                garbage_form,
+                electric_form,
+                cable_form,
+                maid_form,
+                hotel_tax_form,
+                occupancy_form,
+                daily_price_form,
+                submit_time)
+
+
+    //send the POST data back to the server from the client using AJAX. Anything not put in the data variable is not passed in the post.
     $.ajax({
       type: "POST",
       url: "/",
-      data : { 'form_title': title_form, 'form_rent': rent_form },
+      data : { 'form_title': title_form, 'form_rent': rent_form, 'water_form': water_form, 'sewer_form': sewer_form, 'garbage_form': garbage_form, 'electric_form': electric_form, 'cable_form': cable_form, 'maid_form': maid_form, 'hotel_tax_form': hotel_tax_form, 'occupancy_form': occupancy_form, 'daily_price_form': daily_price_form, 'submit_time': submit_time
+      },
+
+      // if the ajax connection was succesfuly we pass the data back in a json object named results
       success: function(results) {
         console.log(results);
 
         // finds the dom item with an id="results" - results (the variable) holds our data from the home funciton in view. We can access jsonify data with results.total
-        $('#results').html(results.total)
+        $('#breakeven').html(results.breakeven)
+        $('#revenue').html(results.revenue)
+        $('#profit').html(results.profit)
+        $('#submit_time').html(results.time_submitted)
+
+        // This clears all input fields after the form submission so the user can easily start over.
         $('input').val('')
       },
       error: function(error) {
--- my_app/views.py
+++ my_app/views.py
@@ -2,15 +2,76 @@ from flask import render_template, jsonify, request, Response
 
 from my_app import app
 
+import logging
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+log.addHandler(console_handler)
+
+
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method=='POST':
         # form.get is used to access the data from a form POST request
-        title = int(request.form.get('form_title'))
-        rent = int(request.form.get('form_rent'))
-        total = rent + title
-        data = {"total": str(total)}
+        # we create the below variables to grab the data from the ajax post.
+        title = request.form.get('form_title')
+        rent = float(request.form.get('form_rent'))
+        occupancy = float(request.form.get('occupancy_form'))
+        hotel_tax_percentage = (float(request.form.get('hotel_tax_form'))) / 100
+        #correct later for correct numbers of days in a month
+        days_occupied = float((occupancy/100)) * 30
+        daily_rate = float(request.form.get('daily_price_form'))
+
+        # COSTS - UTILITIES ------------------
+        water = float(request.form.get('water_form'))
+        sewer = float(request.form.get('sewer_form'))
+        garbage = float(request.form.get('garbage_form'))
+        electric = float(request.form.get('electric_form'))
+        cable = float(request.form.get('cable_form'))
+
+        # COSTS - SERVICES ------------------
+        maid = float(request.form.get('form_rent'))
+        # This assumes on average a person stays for 2 days.  Find a better way t0 establish this later.
+        maid_costs_monthly = ((maid * days_occupied) / 2)
+        hotel_tax_reserve_monthly = (daily_rate * hotel_tax_percentage) * days_occupied
+
+        # COSTS - totals
+        utility_costs_monthly = water + sewer + garbage + electric + cable
+        service_costs_monthly = maid_costs_monthly + hotel_tax_reserve_monthly
+        total_cost_monthly = utility_costs_monthly + service_costs_monthly
+
+
+        # ------------------ OUTPUT ------------------
+        #precautions - hotel tax reserve fund to save per booking
+
+        breakeven = total_cost_monthly
+        revenue = daily_rate * days_occupied
+        profit = revenue - total_cost_monthly
+        time_submitted = request.form.get('submit_time')
+
+
+        # We add the data we want to pass back to a dictionary
+        data = {"breakeven": str(breakeven),
+            "time_submitted": time_submitted,
+            "revenue": revenue,
+            "profit": profit
+            }
+
+        #we return the data to ajax and in turn the html page after serilzing our dictionary into json format.
+        log.info(title)
         return jsonify(data)
 
     #if POST doesn't render - render the below GET instead
     return render_template("calculator.html")
+
+
+#SUMMARY OF DATA FLOW
+# - Grab HTML form posts but we stop it from interacting with the server with onsubmit="return false" in the form tag.
+# - Then using main.js we grab the data in the fields.
+# - Then using ajax we pass this data to our server in a post object.
+# - We make our manupliations on the data in the home view
+# - Any data we want to send back to the page we put into a dictionary and return to ajax.  We then tell ajax to insert the data into span tags we have identified.
