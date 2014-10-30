a9950b721abd66683a8a1a72d2f1c2e60dd173ef
Josh Wardini <joshwardini@gmail.com>
2014-10-30 20:45:19 +0800

    update

--- my_app/config.py
+++ my_app/config.py
@@ -1,7 +1,7 @@
 import os
 class DevelopmentConfig(object):
-    #if your not accesing heroku, fallback to sqlite
-    SQLALCHEMY_DATABASE_URI = os.environ.get('heroku','sqlite')
+    #os.environ.get trys to access the dictionary key heroku (DATABASE_URL) , fallsback to sqlite url if not
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','sqlite:///airbnb-development.db')
     # SQLALCHEMY_DATABASE_URI = "sqlite:///airbnb-development.db"
     DEBUG = True
     SECRET_KEY = "Not secret"
