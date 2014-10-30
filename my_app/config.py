f13bbabf85f9d28dc1737b29461dbb5bceddde37
Josh Wardini <joshwardini@gmail.com>
2014-10-30 20:41:51 +0800

    changed requiremnets file and configuration file

--- my_app/config.py
+++ my_app/config.py
@@ -1,6 +1,7 @@
 import os
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgres://talzrazzwolyoi:roDY1kg7PgN1tX9LyWP6Nlex7U@ec2-54-243-42-236.compute-1.amazonaws.com:5432/d61lneaff8ggt2"
+    #if your not accesing heroku, fallback to sqlite
+    SQLALCHEMY_DATABASE_URI = os.environ.get('heroku','sqlite')
     # SQLALCHEMY_DATABASE_URI = "sqlite:///airbnb-development.db"
     DEBUG = True
     SECRET_KEY = "Not secret"
